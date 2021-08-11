0f4c72a17e9a77860fd1f31b00bf8410a318a897
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2021-08-11 20:09:18 +0200

    Setup the project, including tests and dependences.

--- src/ANema.jl
+++ src/ANema.jl
@@ -1,5 +1,8 @@
 module ANema
-
 include("evtfn.jl")
+include("evtdf.jl")
+include("dftolor.jl")
 
+export setunits, write_lors_hdf5, radial_correcction,deltatime,cdoi,ctsr, crt, dftolor
+export readdf, writemdf, selectinterval
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,31 @@
 using ANema
+using ATools 
+using DataFrames
 using Test
 
+dir = "../data/"
+conf = "n3-w-20mm-phot"
+path = joinpath(dir, conf)
+
 @testset "ANema.jl" begin
-    # Write your tests here.
+    ndf = readdf(path) 
+    @test nrow(ndf) == 180079
+    @test length(names(ndf)) ==  46
+
+    ndfq =selectinterval(ndf, "q1", "q2", 1700.0, 2100.0);
+    @test nrow(ndfq) == 42875
+
+    ndfz =selectinterval(ndfq, "zstd1", "zstd2", 1.0, 16.0)
+    @test nrow(ndfz) == 42806
+
+    fz, pz = fit_profile(ndfz.zstd1, ndfz.r1, "σz", "r", "pol2")
+    fpars = fz.fitpar
+    fstds = fz.fitstd
+    @test isapprox(fpars[1], 372.96, atol=0.1) 
+
+    r1z = fz.g.(ndfz.zstd1)
+    fg,p = fitg1(r1z - ndfz.r1, "r1 - r1z", 200, -5.0, 5.0, xgmin=-1.5, xgmax=1.5)
+    @test isapprox(fg.std[1], 0.4, atol=0.1) 
+    
+ 
 end
