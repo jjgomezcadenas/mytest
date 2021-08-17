7ceac3d77feccb1fbb2963282c2fd16facadb029
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2021-08-17 15:55:49 +0200

    Expand API.

--- src/ANema.jl
+++ src/ANema.jl
@@ -2,7 +2,10 @@ module ANema
 include("evtfn.jl")
 include("evtdf.jl")
 include("dftolor.jl")
+include("nema4.jl")
 
-export setunits, write_lors_hdf5, radial_correcction,deltatime,cdoi,ctsr, crt, dftolor
+export setunits, write_lors_hdf5, radial_correcction, 
+       deltatime, cdoi, ctsr, crt, dftolor
 export readdf, writemdf, selectinterval
+export distance_lor_to_point, sinogramdf, ztsinogram, thetasgrm, zsgrm 
 end
--- src/dftolor.jl
+++ src/dftolor.jl
@@ -15,15 +15,16 @@ import Unitful:
 
 Struct representing a LOR
 """
-struct MlemLor
-    dx::Float32
-    x1::Float32
-    y1::Float32
-    z1::Float32
-    x2::Float32
-    y2::Float32
-    z2::Float32
+struct MlemLor{T}
+    dx::T
+    x1::T
+    y1::T
+    z1::T
+    x2::T
+    y2::T
+    z2::T
 end
+
 
 """
 	write_lors_hdf5(filename, mlor)
@@ -31,21 +32,21 @@ end
 """
 function write_lors_hdf5(filename, mlor)
 
-    function set_datatype(::Type{MlemLor})
-        dtype = HDF5.h5t_create(HDF5.H5T_COMPOUND, sizeof(MlemLor))
-        HDF5.h5t_insert(dtype, "dx", fieldoffset(MlemLor, 1),
+    function set_datatype(::Type{MlemLor{Float32}})
+        dtype = HDF5.h5t_create(HDF5.H5T_COMPOUND, sizeof(MlemLor{Float32}))
+        HDF5.h5t_insert(dtype, "dx", fieldoffset(MlemLor{Float32}, 1),
                         datatype(Float32))
-        HDF5.h5t_insert(dtype, "x1", fieldoffset(MlemLor, 2),
+        HDF5.h5t_insert(dtype, "x1", fieldoffset(MlemLor{Float32}, 2),
                         datatype(Float32))
-        HDF5.h5t_insert(dtype, "y1", fieldoffset(MlemLor, 3),
+        HDF5.h5t_insert(dtype, "y1", fieldoffset(MlemLor{Float32}, 3),
                         datatype(Float32))
-        HDF5.h5t_insert(dtype, "z1", fieldoffset(MlemLor, 4),
+        HDF5.h5t_insert(dtype, "z1", fieldoffset(MlemLor{Float32}, 4),
                         datatype(Float32))
-        HDF5.h5t_insert(dtype, "x2", fieldoffset(MlemLor, 5),
+        HDF5.h5t_insert(dtype, "x2", fieldoffset(MlemLor{Float32}, 5),
                         datatype(Float32))
-        HDF5.h5t_insert(dtype, "y2", fieldoffset(MlemLor, 6),
+        HDF5.h5t_insert(dtype, "y2", fieldoffset(MlemLor{Float32}, 6),
                         datatype(Float32))
-        HDF5.h5t_insert(dtype, "z2", fieldoffset(MlemLor, 7),
+        HDF5.h5t_insert(dtype, "z2", fieldoffset(MlemLor{Float32}, 7),
                         datatype(Float32))
 
         HDF5.Datatype(dtype)
@@ -53,7 +54,7 @@ function write_lors_hdf5(filename, mlor)
 
     h5f = h5open(filename, "w")
 
-    dtype = set_datatype(MlemLor)
+    dtype = set_datatype(MlemLor{Float32})
     dspace = dataspace(mlor)
     grp = create_group(h5f, "true_info")
     dset = create_dataset(grp, "lors", dtype, dspace)
@@ -139,50 +140,6 @@ function cdoi(df::DataFrame, position::Possel=postrue, nlxe::Number=1.6)
     return uconvert.(ps, (dxrb2 - dxrb1)/clxe)
 end
 
-# function cdoi(df::DataFrame, r1::Vector{Float64}, r2::Vector{Float64},
-#               position::Possel=postrue, nlxe::Number=1.6)
-#     clxe = SpeedOfLightInVacuum/nlxe
-
-#     if position == posreco
-#         dxrb1 = [dxyz([df.x1[i], df.y1[i], df.z1[i]],
-#                               [df.xb1[i], df.yb1[i], df.zb1[i]]) for i in 1:nrow(df)]
-
-#         dxrb2 = [dxyz([df.x2[i], df.y2[i], df.z2[i]],
-#                               [df.xb2[i], df.yb2[i], df.zb2[i]]) for i in 1:nrow(df)]
-
-#     elseif position == posrecall
-
-#         xq1,yq1,zq1 =  radial_correction(df.xr1./mm, df.yr1./mm, df.zr1./mm, r1)
-#         xq2,yq2,zq2 =  radial_correction(df.xr2./mm, df.yr2./mm, df.zr2./mm, r2)
-
-#         dxrb1 = [dxyz([xq1[i]*mm, yq1[i]*mm, zq1[i]*mm],
-#                               [df.xb1[i], df.yb1[i], df.zb1[i]]) for i in 1:nrow(df)]
-
-#         dxrb2 = [dxyz([xq2[i]*mm, yq2[i]*mm, zq2[i]*mm],
-#                               [df.xb2[i], df.yb2[i], df.zb2[i]]) for i in 1:nrow(df)]
-
-#     else
-#         dxrb1 = [dxyz([df.xt1[i], df.yt1[i], df.zt1[i]],
-#                               [df.xb1[i], df.yb1[i], df.zb1[i]]) for i in 1:nrow(df)]
-
-#         dxrb2 = [dxyz([df.xt2[i], df.yt2[i], df.zt2[i]],
-#                               [df.xb2[i], df.yb2[i], df.zb2[i]]) for i in 1:nrow(df)]
-
-#     end
-#     return uconvert.(ps, (dxrb2 - dxrb1)/clxe)
-# end
-
-
-# function cdoi(df::DataFrame, position::Possel=postrue, reco="phistd", nlxe::Number=1.6)
-#     if reco == "phistd"
-#         return cdoi(df, df.r1p./mm , df.r2p./mm, position, nlxe)
-#     elseif reco == "zstd"
-#         return cdoi(df, df.r1z./mm , df.r2z./mm, position, nlxe)
-#     else
-#         return cdoi(df, df.r1./mm , df.r2./mm, position, nlxe)
-#     end
-# end
-
 
 function ctsr(df::DataFrame, position::Possel=postrue)
     cc = float(SpeedOfLightInVacuum)
@@ -235,23 +192,52 @@ Take dataframe df and return a vector of MlemLor.
 Since MlemLor will be written to file, remove units (e.g, use implicit units, in this
 case mm) and transform to Float32
 """
-function dftolor(df::DataFrame, t::Dtsel=dtfirst, position::Possel=postrue)
+function dftolor(df::DataFrame, t::Dtsel=dtfirst, position::Possel=postrue; f32::Bool=true)
 
     function tof32(l)
         Float32.(l/mm)
     end
 
+    function tof64(l)
+        Float64.(l/mm)
+    end
+
+    @debug "f32 =", f32
+
     dx    = dxf(df, t, position)
+
     if position == postrue
         x1, x2, y1, y2, z1, z2 = df.xt1, df.xt2, df.yt1, df.yt2, df.zt1, df.zt2
-        return MlemLor.(tof32(dx),tof32(x1),tof32(y1),tof32(z1), tof32(x2), tof32(y2), tof32(z2))
+        if f32
+            return MlemLor{Float32}.(tof32(dx),tof32(x1),tof32(y1),tof32(z1), 
+                            tof32(x2), tof32(y2), tof32(z2))
+        else
+            return MlemLor{Float64}.(tof64(dx),tof64(x1),tof64(y1),tof64(z1), 
+                            tof64(x2), tof64(y2), tof64(z2))
+        end
     elseif position == posreco
         x1, x2, y1, y2, z1, z2 = df.x1,  df.x2,  df.y1,  df.y2,  df.z1,  df.z2
-        return MlemLor.(tof32(dx),tof32(x1),tof32(y1),tof32(z1), tof32(x2), tof32(y2), tof32(z2))
+        if f32
+            return MlemLor{Float32}.(tof32(dx),tof32(x1),tof32(y1),tof32(z1), 
+                            tof32(x2), tof32(y2), tof32(z2))
+        else
+            return MlemLor{Float64}.(tof64(dx),tof64(x1),tof64(y1),tof64(z1), 
+                            tof64(x2), tof64(y2), tof64(z2))
+        end
     else
         x1,y1,z1 =  radial_correction(df.xr1./mm, df.yr1./mm, df.zr1./mm, df.r1x./mm)
         x2,y2,z2 =  radial_correction(df.xr2./mm, df.yr2./mm, df.zr2./mm, df.r2x./mm)
-        return MlemLor.(tof32(dx),Float32.(x1),Float32.(y1),Float32.(z1), Float32.(x2), Float32.(y2), Float32.(z2))
-    end
+        if f32
+            return MlemLor{Float32}.(tof32(dx),
+                            Float32.(x1),Float32.(y1),Float32.(z1), 
+                            Float32.(x2), Float32.(y2), Float32.(z2))
+        else
+            @debug "f32 =", f32
+            @debug "dx1 =", tof64(dx)[1]
+            @debug "x1  =", Float64.(x1)[1]
 
+            return MlemLor{Float64}.(tof64(dx),Float64.(x1),Float64.(y1),Float64.(z1), 
+                            Float64.(x2), Float64.(y2), Float64.(z2))
+        end
+    end
 end
